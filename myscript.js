fbdfffc52482b7cf70083740343057070f06b224
Daniel Madrigal <dsmadrigal@gmail.com>
2017-12-09 19:35:34 -0800

    getting the weather app location service up and running onto the github page

--- myscript.js
+++ myscript.js
@@ -1,6 +1,19 @@
-var name = prompt("What's your name?");
-
+var name = 'Charlie';
 
 document.write(name);
 
+console.log(name); 
 
+var x = document.getElementById("location");
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude + 
+    "<br>Longitude: " + position.coords.longitude; 
+}

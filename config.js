cf35b67db5a2a80ba334da0a1560b83e0bf59777
Daniel Madrigal <dsmadrigal@gmail.com>
2018-01-03 22:08:38 -0800

    fix config file

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var config = {
-    MY_KEY : '1d8a7e1aefde45b0014cce913957e110'
-    G_KEY: 'AIzaSyBGyUmMtnYiNOfIhvHu0qb_MayJJgAk8b8';
+    MY_KEY : '1d8a7e1aefde45b0014cce913957e110',
+    G_KEY: 'AIzaSyBGyUmMtnYiNOfIhvHu0qb_MayJJgAk8b8'
   }
   
